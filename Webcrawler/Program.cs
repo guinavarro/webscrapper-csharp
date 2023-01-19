95b9a08be633ae2837dbbc1fb745da77911ba286
Navarro <guilherme-navarro@outlook.com>
2023-01-19 09:36:07 -0300

    Deployed

---
+++ Webcrawler/Program.cs
@@ -0,0 +1,13 @@
+﻿using Services;
+
+namespace Webcrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Paginacao paginacao = new Paginacao();
+            paginacao.CrawlUrl();
+        }
+    }
+}
